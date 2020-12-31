0b5e88d727b61c6b4b714a793117c428a65e40e6
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-12-31 18:15:47 +0100

    randomPicker

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value);
-
+    
     if(e.key === 'Enter') {
         setTimeout(() => {
-            e.target.value = '';
+            e.target.value = ''
         }, 10);
 
         randomSelect();
@@ -17,14 +17,14 @@ textarea.addEventListener('keyup', (e) => {
 
 function createTags(input) {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
-    
+
     tagsEl.innerHTML = '';
 
-    tags.forEach(tag => {
+    tags.forEach (tag => {
         const tagEl = document.createElement('span');
         tagEl.classList.add('tag');
-        tagEl.innerText = tag;
-        tagsEl.appendChild(tagEl); 
+        tagEl.innerHTML = tag;
+        tagsEl.appendChild(tagEl);
     });
 }
 
@@ -33,5 +33,34 @@ function randomSelect() {
 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag()
-    })
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unhighlightTag(randomTag);
+        }, 100)
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+
+            highlightTag(randomTag);
+        }, 100)
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight');
 }
